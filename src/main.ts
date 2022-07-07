847dc7f603487cb4023a88b0df6bb91ea9720e3a
VladKrauchuk <krauchukvlad@gmail.com>
2022-07-07 14:14:29 +0300

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+
+	// Swagger doc
+	const config = new DocumentBuilder()
+		.setTitle('FeedBack API')
+		.setVersion('0.1')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('/', app, document);
+
 	app.setGlobalPrefix('api');
 	await app.listen(3000);
 }
